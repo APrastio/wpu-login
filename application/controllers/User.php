025fedc6202cd68e519e578958072df2fda45a2d
APrastio <wersa245@gmail.com>
2020-10-04 12:02:42 +0700

    menambah wpu

---
+++ application/controllers/User.php
@@ -0,0 +1,109 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+    public function index()
+    {
+        $data = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['title'] = 'My Profile';
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function edit()
+    {
+        $data = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['title'] = 'Edit Profile';
+
+        $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+            //cek gamabr
+
+            $upload_image = $_FILES['image']['name'];
+
+            if ($upload_image) {
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '2048';
+                $config['upload_path'] = './assets/img/profile/';
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['image'];
+                    if ($old_image != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+            // $new_image = $this->upload->data('image');
+            // $this->db->set('image', $new_image);
+            $this->db->set('name', $name);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">your Profile has been edited</div>');
+            redirect('user');
+        }
+    }
+
+    public function changepassword()
+    {
+        $data = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['title'] = 'Change Password';
+
+
+        $this->form_validation->set_rules('currentpassword', 'Current Password', 'required|trim');
+        $this->form_validation->set_rules('newpassword1', 'New Password', 'required|trim|min_length[3]|matches[newpassword2]');
+        $this->form_validation->set_rules('newpassword2', 'Confirm New Password', 'required|trim|min_length[3]|matches[newpassword1]');
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/changepassword', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $currentpassword = $this->input->post('currentpassword');
+            $newpassword = $this->input->post('newpassword1');
+            if (!password_verify($currentpassword, $data['password'])) {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Wrong current password!</div>');
+                redirect('user/changepassword');
+            } else {
+                if ($currentpassword == $newpassword) {
+                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">New password can not be the same as current password!  </div>');
+                    redirect('user/changepassword');
+                } else {
+                    $password_hash = password_hash($newpassword, PASSWORD_DEFAULT);
+                    $this->db->set('password', $password_hash);
+                    $this->db->where('email', $this->session->userdata('email'));
+                    $this->db->update('user');
+
+                    $this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">New password has been change!</div>');
+                    redirect('user/changepassword');
+                }
+            }
+        }
+    }
+}
---
+++ application/views/menu/submenu.php
@@ -0,0 +1,104 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+
+    <div class="row">
+        <div class="col-lg">
+            <?php if (validation_errors()) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= validation_errors() ?>
+                </div>
+            <?php endif; ?>
+            <?= $this->session->flashdata('pesan'); ?>
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newSubMenuModal">Add New Submenu</a>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Submenu</th>
+                        <th scope="col">Menu</th>
+                        <th scope="col">Url</th>
+                        <th scope="col">Icon</th>
+                        <th scope="col">Active</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1;
+                    foreach ($subMenu as $sm) : ?>
+                        <tr>
+                            <th scope="row"><?= $i++ ?></th>
+                            <td><?= $sm['title'] ?></td>
+                            <td><?= $sm['menu'] ?></td>
+                            <td><?= $sm['url'] ?></td>
+                            <td><?= $sm['icon'] ?></td>
+                            <td><?= $sm['is_active'] ?></td>
+                            <td>
+                                <a href="" class="badge badge-success">Edit</a>
+                                <a href="" class="badge badge-danger">Delete</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="newSubMenuModal" tabindex="-1" role="dialog" aria-labelledby="newSubMenuModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newSubMenuModalLabel">Add New Submenu</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= base_url('menu/submenu') ?>" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="title" placeholder="Submenu title" name="title">
+                    </div>
+                    <div class="form-group">
+                        <select name="menu_id" id="menu_id" class="form-control">
+                            <option value="">Select Menu</option>
+                            <?php foreach ($menu as $m) : ?>
+                                <option value="<?= $m['id'] ?>"><?= $m['menu'] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="url" placeholder="Submenu url" name="url">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="icon" placeholder="Submenu icon" name="icon">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="1" id="is_active" name="is_active" checked>
+                            <label class="form-check-label" for="is_active">
+                                Active?
+                            </label>
+                        </div>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">add</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
